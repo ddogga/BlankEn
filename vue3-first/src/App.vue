53612b560cec75d042733e1b3539f7c2b354fd58
ddogga <hayeonlee60327@gmail.com>
2022-12-06 18:13:34 +0900

    'merge'

---
+++ vue3-first/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Nav/>
+    <Main/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Main from './views/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+    Main,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
