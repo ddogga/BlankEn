f7ff09d7c69e6739b911346dc5c4c7f48155c0c3
ddogga <hayeonlee60327@gmail.com>
2022-12-07 23:27:05 +0900

    헤더부분 nav 단어 수정

--- vue3-first/src/components/Header.vue
+++ vue3-first/src/components/Header.vue
@@ -2,7 +2,7 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#!">Start Bootstrap</a>
+            <router-link class="navbar-brand" to="/">BlankEN</router-link>
             <button
             class="navbar-toggler"
             type="button"
@@ -17,10 +17,15 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#!">Home</a>
+                <a class="nav-link active" aria-current="page" href="#!">퀴즈 풀기</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                <li class="nav-item dropdown">
+            </ul>
+            <form class="d-flex">
+                <button class="btn btn-outline-dark" type="submit">
+                로그인
+                </button>
+            </form>
+            <div class="nav-item dropdown">
                 <a
                     class="nav-link dropdown-toggle"
                     id="navbarDropdown"
@@ -28,7 +33,7 @@
                     role="button"
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
-                    >Shop</a
+                    >ID</a
                 >
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="#!">All Products</a></li>
@@ -36,15 +41,7 @@
                     <li><a class="dropdown-item" href="#!">Popular Items</a></li>
                     <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
                 </ul>
-                </li>
-            </ul>
-            <form class="d-flex">
-                <button class="btn btn-outline-dark" type="submit">
-                <i class="bi-cart-fill me-1"></i>
-                Cart
-                <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                </button>
-            </form>
+            </div>
             </div>
         </div>
     </nav>
