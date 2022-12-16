24f340a731b1bc955be3f0622a075d4a68f3848e
ddogga <hayeonlee60327@gmail.com>
2022-12-16 22:28:34 +0900

    BoardList에 정렬방법을 선택할 수 있는 드롭박스 생성

--- vue3-first/src/components/Header.vue
+++ vue3-first/src/components/Header.vue
@@ -1,60 +1,60 @@
 <template>
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container px-4 px-lg-5">
-            <router-link class="navbar-brand" to="/">BlankEN</router-link>
-            <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+  <!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container px-4 px-lg-5">
+      <router-link class="navbar-brand" to="/">BlankEN</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/blanken/boardlist"
             >
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                <li class="nav-item">
-                    <router-link class="nav-link active" aria-current="page" to="/blanken/boardlist">
-                        퀴즈 풀기
-                    </router-link>
-                </li>
-            </ul>
-            <form class="d-flex">
-                <button class="btn btn-outline-dark" type="submit">
-                로그인
-                </button>
-            </form>
-            <div class="nav-item dropdown">
-                <a
-                    class="nav-link dropdown-toggle"
-                    id="navbarDropdown"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                    >ID</a
-                >
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">All Products</a></li>
-                    <li><hr class="dropdown-divider" /></li>
-                    <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                    <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
-                </ul>
-            </div>
-            </div>
+              퀴즈 풀기
+            </router-link>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <button class="btn btn-outline-dark" type="submit">로그인</button>
+        </form>
+        <div class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            id="navbarDropdown"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            >ID</a
+          >
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#!">All Products</a></li>
+            <li><hr class="dropdown-divider" /></li>
+            <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+            <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+          </ul>
         </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-import {useRouter} from 'vue-router';
+import { useRouter } from "vue-router";
 
 export default {
-    setup() {
-        
-    },
-}
+  setup() {},
+};
 </script>
--- vue3-first/src/views/board/BoardList.vue
+++ vue3-first/src/views/board/BoardList.vue
@@ -1,264 +1,297 @@
 <template>
-    <section class="py-5">
-        <div class="container px-4 px-lg-5 mt-5">
-            <div
-            class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
-            >
-                <div class="dropdown mb-5">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                    <ul class="dropdown-menu" v-on:click.stop="doThis">
-                        <li class="dropdown-item"><label>모두<input type="checkbox" v-model="selectAll" value="all" @change="all"></label></li>
-                        <li class="dropdown-item"><label>드라마<input type="checkbox" v-model="selectedValues" value="드라마"></label></li>
-                        <li class="dropdown-item"><label>여행<input type="checkbox" v-model="selectedValues" value="여행"></label></li>
-                        <li class="dropdown-item"><label>일상회화<input type="checkbox" v-model="selectedValues" value="일상회화"></label></li>
-                        <li class="dropdown-item"><label>비즈니스<input type="checkbox" v-model="selectedValues" value="비즈니스"></label></li>
-                        <li class="dropdown-item"><label>오픽/토스<input type="checkbox" v-model="selectedValues" value="오픽/토스"></label></li>
-                    </ul>
-                </div>
-            </div>
-
-            <div
-            class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
-            >
-                <ul class="tag-list">
-                    <li class="tag-item" v-for="(item,index) in selectedValues" :key="index">
-                        <sapn class="badge bg-success text-white tag" >
-                            {{item}}
-                        </sapn>
-                    </li>
-                </ul>
-                
-            </div>
-            
-
-            <div
-            class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
-            >
-                <table>
-                    <caption>Developers Rating</caption>
-                    <thead>
-                        <tr>
-                            <th>상태</th>
-                            <th colspan="2">제목</th>
-                            <th>문제수</th>
-                            <th>카테고리</th>
-                            <th>추천수</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td rowspan="2"><sapn
-                            class="badge bg-success text-white"
-                            >완료</sapn></td>
-                            <td>제목</td>
-                            <td rowspan="2">버튼</td>
-                            <td rowspan="2">David</td>
-                            <td rowspan="2">180</td>
-                            <td rowspan="2">
-                                <button class="view">view</button>
-                                <button class="delete">Delete</button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>아이디</td>
-                        </tr>
-
-
-                        <tr>
-                            <td rowspan="2"><sapn
-                            class="badge bg-secondary text-white"
-                            >진행중</sapn></td>
-                            <td>제목</td>
-                            <td rowspan="2">버튼</td>
-                            <td rowspan="2">David</td>
-                            <td rowspan="2">180</td>
-                            <td rowspan="2">
-                                <button class="view">view</button>
-                                <button class="delete">Delete</button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>아이디</td>
-                        </tr>
-
-
-                        <tr>
-                            <td rowspan="2"><sapn
-                            class="badge bg-secondary text-white"
-                            >진행중</sapn></td>
-                            <td>제목</td>
-                            <td rowspan="2">버튼</td>
-                            <td rowspan="2">David</td>
-                            <td rowspan="2">180</td>
-                            <td rowspan="2">
-                                <button class="view">view</button>
-                                <button class="delete">Delete</button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>아이디</td>
-                        </tr>
-
-                                                <tr>
-                            <td rowspan="2"><sapn
-                            class="badge bg-secondary text-white"
-                            >진행중</sapn></td>
-                            <td>제목</td>
-                            <td rowspan="2">버튼</td>
-                            <td rowspan="2">David</td>
-                            <td rowspan="2">180</td>
-                            <td rowspan="2">
-                                <button class="view">view</button>
-                                <button class="delete">Delete</button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>아이디</td>
-                        </tr>
-
-
-                        <tr>
-                            <td rowspan="2"><sapn
-                            class="badge bg-secondary text-white"
-                            >진행중</sapn></td>
-                            <td>제목</td>
-                            <td rowspan="2">버튼</td>
-                            <td rowspan="2">David</td>
-                            <td rowspan="2">180</td>
-                            <td rowspan="2">
-                                <button class="view">view</button>
-                                <button class="delete">Delete</button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>아이디</td>
-                        </tr>
-
-                                                <tr>
-                            <td rowspan="2"><sapn
-                            class="badge bg-secondary text-white"
-                            >진행중</sapn></td>
-                            <td>제목</td>
-                            <td rowspan="2">버튼</td>
-                            <td rowspan="2">David</td>
-                            <td rowspan="2">180</td>
-                            <td rowspan="2">
-                                <button class="view">view</button>
-                                <button class="delete">Delete</button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>아이디</td>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-                        <td colspan="5" class="tablefoot"></td>
-                    </tfoot>
-                </table>
-            </div>
+  <section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+      <!-- 검색 drop-box -->
+      <div
+        class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
+      >
+        <div class="dropdown mb-5">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            카테고리
+          </button>
+          <ul class="dropdown-menu" v-on:click.stop="doThis">
+            <li class="dropdown-item">
+              <label
+                >모두<input
+                  type="checkbox"
+                  v-model="selectAll"
+                  value="all"
+                  @change="all"
+              /></label>
+            </li>
+            <li class="dropdown-item">
+              <label
+                >드라마<input
+                  type="checkbox"
+                  v-model="selectedValues"
+                  value="드라마"
+              /></label>
+            </li>
+            <li class="dropdown-item">
+              <label
+                >여행<input
+                  type="checkbox"
+                  v-model="selectedValues"
+                  value="여행"
+              /></label>
+            </li>
+            <li class="dropdown-item">
+              <label
+                >일상회화<input
+                  type="checkbox"
+                  v-model="selectedValues"
+                  value="일상회화"
+              /></label>
+            </li>
+            <li class="dropdown-item">
+              <label
+                >비즈니스<input
+                  type="checkbox"
+                  v-model="selectedValues"
+                  value="비즈니스"
+              /></label>
+            </li>
+            <li class="dropdown-item">
+              <label
+                >오픽/토스<input
+                  type="checkbox"
+                  v-model="selectedValues"
+                  value="오픽/토스"
+              /></label>
+            </li>
+          </ul>
         </div>
-        </section>
+      </div>
+
+      <!-- 검색 태그 -->
+      <div
+        class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
+      >
+        <ul class="tag-list">
+          <li
+            class="tag-item"
+            v-for="(item, index) in selectedValues"
+            :key="index"
+          >
+            <sapn class="badge bg-success text-white tag">
+              {{ item }}
+            </sapn>
+          </li>
+        </ul>
+      </div>
+
+      <nav class="navbar navbar-expand-lg">
+        <div class="container gx-4 px-lg-5" style="width: 1000px">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+              문제수 : <span>30</span>
+            </div>
+            <div class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                id="navbarDropdown"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                >최신순</a
+              >
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#!">추천순</a></li>
+                <li><a class="dropdown-item" href="#!">최신순</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <!-- 검색 리스트 -->
+      <div
+        class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
+      >
+        <table>
+          <caption>
+            Developers Rating
+          </caption>
+          <thead>
+            <tr>
+              <th>상태</th>
+              <th colspan="2">제목</th>
+              <th>문제수</th>
+              <th>카테고리</th>
+              <th>추천수</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td rowspan="2">
+                <sapn class="badge bg-success text-white">완료</sapn>
+              </td>
+              <td>제목</td>
+              <td rowspan="2">버튼</td>
+              <td rowspan="2">David</td>
+              <td rowspan="2">180</td>
+              <td rowspan="2">number</td>
+            </tr>
+
+            <tr>
+              <td>아이디</td>
+            </tr>
+
+            <tr>
+              <td rowspan="2">
+                <sapn class="badge bg-secondary text-white">진행중</sapn>
+              </td>
+              <td>제목</td>
+              <td rowspan="2">버튼</td>
+              <td rowspan="2">David</td>
+              <td rowspan="2">180</td>
+              <td rowspan="2"></td>
+            </tr>
+
+            <tr>
+              <td>아이디</td>
+            </tr>
+
+            <tr>
+              <td rowspan="2">
+                <sapn class="badge bg-secondary text-white">진행중</sapn>
+              </td>
+              <td>제목</td>
+              <td rowspan="2">버튼</td>
+              <td rowspan="2">David</td>
+              <td rowspan="2">180</td>
+              <td rowspan="2"></td>
+            </tr>
+
+            <tr>
+              <td>아이디</td>
+            </tr>
+
+            <tr>
+              <td rowspan="2">
+                <sapn class="badge bg-secondary text-white">진행중</sapn>
+              </td>
+              <td>제목</td>
+              <td rowspan="2">버튼</td>
+              <td rowspan="2">David</td>
+              <td rowspan="2">180</td>
+              <td rowspan="2"></td>
+            </tr>
+
+            <tr>
+              <td>아이디</td>
+            </tr>
+
+            <tr>
+              <td rowspan="2">
+                <sapn class="badge bg-secondary text-white">진행중</sapn>
+              </td>
+              <td>제목</td>
+              <td rowspan="2">버튼</td>
+              <td rowspan="2">David</td>
+              <td rowspan="2">180</td>
+              <td rowspan="2"></td>
+            </tr>
+
+            <tr>
+              <td>아이디</td>
+            </tr>
+
+            <tr>
+              <td rowspan="2">
+                <sapn class="badge bg-secondary text-white">진행중</sapn>
+              </td>
+              <td>제목</td>
+              <td rowspan="2">버튼</td>
+              <td rowspan="2">David</td>
+              <td rowspan="2">180</td>
+              <td rowspan="2"></td>
+            </tr>
+
+            <tr>
+              <td>아이디</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-    import {ref} from 'vue';
-    export default {
-        setup() {
+import { ref } from "vue";
+export default {
+  setup() {
+    const selectAll = ref(false);
+    const selectedValues = ref([]);
+    const sortValue = ref("최신순");
 
-            const selectAll = ref(false);
-            const selectedValues = ref([])
+    //카테고리 전체 선택
+    const all = () => {
+      if (selectAll.value) {
+        selectedValues.value = [
+          "드라마",
+          "여행",
+          "일상회화",
+          "비즈니스",
+          "오픽/토스",
+        ];
+      } else {
+        selectedValues.value = [];
+      }
+    };
 
-
-            //카테고리 전체 선택
-            const all = () =>{
-                if(selectAll.value){
-                    selectedValues.value = ["드라마","여행","일상회화","비즈니스","오픽/토스"]
-                } else{
-                    selectedValues.value = [];
-                }
-            }
-
-            return{
-                selectAll,selectedValues,all
-            }
-        },
-    }
+    return {
+      selectAll,
+      selectedValues,
+      all,
+    };
+  },
+};
 </script>
-
-
 
 <style scoped>
 /* @import "../../assets/css/bootstrap.css"; */
 
-
 /*검색 태그 */
 
 .tag-list {
-    list-style: none;
+  list-style: none;
 }
 .tag-item {
-    float: left;
-    margin-right: 20px;
+  float: left;
+  margin-right: 20px;
 }
-
 
 /*table css */
 table {
-    width: 700px;
-    text-align: center;
-    border: 1px solid #fff;
-    border-spacing: 1px;
-    font-family: 'Cairo', sans-serif;
+  width: 1000px;
+  text-align: center;
+  border: 1px solid #fff;
+  border-spacing: 1px;
+  font-family: "Cairo", sans-serif;
   margin: auto;
 }
 
 caption {
-    font-weight: bold;
+  font-weight: bold;
 }
 
 table td {
-    padding: 10px;
-    background-color: #eee;
+  padding: 10px;
+  background-color: #eee;
 }
 
 table th {
-    background-color: #333;
-    color: #fff;
-    padding: 10px;
-}
-
-img {
-    width: 90px;
-    height: 90px;
-}
-
-.view,
-.delete {
-    border: none;
-    padding: 5px 10px;
-    color: #fff;
-    font-weight: bold;
-}
-
-.view {
-    background-color: #03A9F4;
-}
-
-.delete {
-    background-color: #E91E63;
-}
-
-.tablefoot {
-    padding: 0;
-    border-bottom: 3px solid #009688;
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
 }
 
 /* dropbox css */
@@ -268,8 +301,4 @@ img {
 .dropdown-menu li input[type="checkbox"] + label { width:17px; height: 17px; background: #fa3062; cursor: pointer; border-radius: 3px;float: right;}
 .dropdown-menu li input[type="checkbox"]:checked + label {background:url(../../assets/images/check.png) #fa3062 no-repeat center/10px 10px; float: right}
 .dropdown-menu li input[type="checkbox"] + label span {position: absolute; left:0px; display: block; } */
-
-
-
-
 </style>
