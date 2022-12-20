b9f94c198e46b87460f766c47fe2d255212c6fc1
ddogga <hayeonlee60327@gmail.com>
2022-12-20 19:47:41 +0900

    홈화면에 퀴즈만들기 버튼 생성

--- vue3-first/src/views/Main.vue
+++ vue3-first/src/views/Main.vue
@@ -10,6 +10,16 @@
           </p>
         </div>
       </div>
+      <div class="container">
+        <router-link class="nav-link active" aria-current="page" to="/">
+          <button
+            style="margin: 20px; outline: none; color: white"
+            class="custom-btn btn-12"
+          >
+            <span>퀴즈 만들기</span><span>Go to make QuizSet</span>
+          </button>
+        </router-link>
+      </div>
     </header>
     <!-- Section-->
     <section class="py-5">
@@ -325,4 +335,75 @@ export default {
 
 <style>
 @import "../assets/css/main.css";
+
+.custom-btn {
+  width: 200px;
+  height: 40px;
+  padding: 10px 25px;
+  border: 2px solid rgb(255, 255, 255);
+  font-family: "Lato", sans-serif;
+  font-weight: 500;
+  background: transparent;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+}
+
+/* 12 */
+.btn-12 {
+  position: relative;
+  right: 20px;
+  bottom: 20px;
+  border: none;
+  width: 200px;
+  height: 40px;
+  line-height: 40px;
+  -webkit-perspective: 230px;
+  perspective: 230px;
+}
+.btn-12 span {
+  display: block;
+  position: absolute;
+  width: 200px;
+  height: 40px;
+  border: 2px solid rgb(255, 255, 255);
+  margin: 0;
+  text-align: center;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+}
+.btn-12 span:nth-child(1) {
+  box-shadow: -7px -7px 20px 0px #fff9, -4px -4px 5px 0px #fff9,
+    7px 7px 20px 0px #0002, 4px 4px 5px 0px #0001;
+  -webkit-transform: rotateX(90deg);
+  -moz-transform: rotateX(90deg);
+  transform: rotateX(90deg);
+  -webkit-transform-origin: 50% 50% -20px;
+  -moz-transform-origin: 50% 50% -20px;
+  transform-origin: 50% 50% -20px;
+}
+.btn-12 span:nth-child(2) {
+  -webkit-transform: rotateX(0deg);
+  -moz-transform: rotateX(0deg);
+  transform: rotateX(0deg);
+  -webkit-transform-origin: 50% 50% -20px;
+  -moz-transform-origin: 50% 50% -20px;
+  transform-origin: 50% 50% -20px;
+}
+.btn-12:hover span:nth-child(1) {
+  -webkit-transform: rotateX(0deg);
+  -moz-transform: rotateX(0deg);
+  transform: rotateX(0deg);
+}
+.btn-12:hover span:nth-child(2) {
+  background: #e0e5ec;
+  color: #e0e5ec;
+  -webkit-transform: rotateX(-90deg);
+  -moz-transform: rotateX(-90deg);
+  transform: rotateX(-90deg);
+}
 </style>
