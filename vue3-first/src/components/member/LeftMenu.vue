6f3a6753ca049f66e4f94021830bb4c33aa0a5b4
ddogga <hayeonlee60327@gmail.com>
2022-12-27 21:33:26 +0900

    add accordian box to leftmenu

--- vue3-first/src/components/member/LeftMenu.vue
+++ vue3-first/src/components/member/LeftMenu.vue
@@ -1,51 +1,69 @@
 <template>
   <div>
-    <!--Main Navigation-->
-    <header>
-      <!-- Sidebar -->
-      <nav id="sidebarMenu" class="d-lg-block sidebar collapse bg-white">
-        <div>
-          <div class="list-group list-group-flush mx-3 mt-4">
-            <!-- Collapse 1 -->
-            <a
-              class="list-group-item list-group-item-action py-2 ripple"
-              href="#collapseExample1"
-              ><span>프로필</span>
-            </a>
-            <!-- Collapse 2 -->
-            <a
-              class="list-group-item list-group-item-action py-2 ripple"
-              href="#collapseExample1"
-              ><span>비밀 번호 변경</span>
-            </a>
-            <!-- Collapse 3 -->
-            <a
-              class="list-group-item list-group-item-action py-2 ripple"
-              href="#collapseExample1"
-              ><span>보관함</span>
-            </a>
-            <!-- Collapsed content -->
-            <ul class="collapse show list-group list-group-flush">
-              <li class="list-group-item py-1">
-                <a href="" class="text-reset">Link</a>
-              </li>
-              <li class="list-group-item py-1">
-                <a href="" class="text-reset">Link</a>
-              </li>
-              <li class="list-group-item py-1">
-                <a href="" class="text-reset">Link</a>
-              </li>
-              <li class="list-group-item py-1">
-                <a href="" class="text-reset">Link</a>
-              </li>
-            </ul>
-            <!-- Collapse 3 -->
+    <!-- Sidebar -->
+    <nav id="sidebarMenu" class="d-lg-block sidebar collapse">
+      <div>
+        <div
+          style="border: 2px black solid"
+          class="list-group list-group-flush mx-3 mt-4"
+        >
+          <!-- Collapse 1 -->
+          <a
+            class="list-group-item list-group-item-action py-2 ripple"
+            href="#collapseExample1"
+            ><span>프로필</span>
+          </a>
+          <!-- Collapse 2 -->
+          <a
+            class="list-group-item list-group-item-action py-2 ripple"
+            href="#collapseExample1"
+            ><span>비밀 번호 변경</span>
+          </a>
+          <!-- Collapse 3 -->
+
+          <!-- Collapsed content -->
+          <div class="list-group-item accordion-item">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              보관함
+            </button>
+
+            <div
+              id="flush-collapseOne"
+              class="accordion-collapse collapse"
+              aria-labelledby="flush-headingOne"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+                <ul class="collapse show list-group list-group-flush">
+                  <li class="list-group-item py-1">
+                    <a href="" class="text-reset">내 퀴즈 목록</a>
+                  </li>
+                  <li class="list-group-item py-1">
+                    <a href="" class="text-reset">히스토리</a>
+                  </li>
+                  <li class="list-group-item py-1">
+                    <a href="" class="text-reset">완료한 퀴즈</a>
+                  </li>
+                  <li class="list-group-item py-1">
+                    <a href="" class="text-reset">좋아요 목록</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
+
+          <!-- Collapse 3 -->
         </div>
-      </nav>
-      <!-- Sidebar -->
-    </header>
-    <!--Main Navigation-->
+      </div>
+    </nav>
+    <!-- Sidebar -->
 
     <!--Main layout-->
     <main style="margin-top: 58px">
@@ -62,11 +80,15 @@ export default {
 </script>
 
 <style scoped>
+a {
+  text-decoration-line: none;
+}
+
 /* Sidebar */
 .sidebar {
   position: fixed;
-  padding: 58px 0 0; /* Height of navbar */
-  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 5%), 0 2px 10px 0 rgb(0 0 0 / 5%);
+  /* background-color: salmon; */
+  margin-top: 58px; /* Height of navbar */
   width: 240px;
 }
 
