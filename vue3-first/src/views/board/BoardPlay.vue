8bbc0ab47de5e17e27fa1b7f277a64a9eb60e52b
ddogga <hayeonlee60327@gmail.com>
2023-01-03 21:29:44 +0900

    Make toggle star button

--- vue3-first/src/views/board/BoardPlay.vue
+++ vue3-first/src/views/board/BoardPlay.vue
@@ -5,9 +5,12 @@
 
       <div class="col mb-5" id="card-row">
         <div class="container card">
-          <nav class="navbar navbar-expand-lg star-btn">
+          <nav
+            class="navbar navbar-expand-lg star-btn"
+            @click.prevent="check_star"
+          >
             <div class="container gx-4 px-lg-5"></div>
-            <div>
+            <div v-if="!star_check">
               <svg
                 fill="#000000"
                 height="30px"
@@ -29,6 +32,27 @@
                 </g>
               </svg>
             </div>
+            <div div v-if="star_check">
+              <svg
+                height="30px"
+                width="30px"
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 47.94 47.94"
+                xml:space="preserve"
+                fill="#000000"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    style="fill: #ed8a19"
+                    d="M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757 c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042 c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685 c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528 c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956 C22.602,0.567,25.338,0.567,26.285,2.486z"
+                  ></path>
+                </g>
+              </svg>
+            </div>
           </nav>
 
           <div class="card-img-top">
@@ -36,10 +60,6 @@
           </div>
           <!-- 한국어 뜻 -->
           <div class="card-body">
-            <div class="arrows">
-              <div id="arrow_left"></div>
-              <div id="arrow_right"></div>
-            </div>
             <div class="korean-input">
               <div class="card-input form-field">
                 <input
@@ -83,6 +103,11 @@
               <button class="delete">정답보기</button>
             </div>
 
+            <div class="arrows">
+              <div id="arrow_left"></div>
+              <div id="arrow_right"></div>
+            </div>
+
             <div class="page_view">1/20</div>
           </div>
         </div>
@@ -90,6 +115,24 @@
     </div>
   </section>
 </template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const star_check = ref(false);
+
+    const check_star = () => {
+      star_check.value = !star_check.value;
+    };
+
+    return {
+      star_check,
+      check_star,
+    };
+  },
+};
+</script>
 
 <style scoped>
 @import "../../assets/css/write.css";
