8bcc878836d5a428fcb242835f5f6bbe7a93fb29
ddogga <hayeonlee60327@gmail.com>
2023-02-01 23:44:36 +0900

    네이버 아이콘 이미지 추가,Redirect.vue추가

---
+++ vue3-first/src/views/member/Redirect.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+
+export default {
+  setup() {
+    const router = useRouter();
+
+    // 컴포넌트 렌더링이 되었을 때,
+
+    // 쿼리스트링으로부터 토큰을 획득
+    const token = this.$route.query.token;
+    console.log("token", token);
+
+    // 토큰이 존재하는 경우, Vuex Store에 토큰을 저장한다.
+    if (token) {
+      this.setToken(token);
+      this.fetchUser();
+    }
+
+    // 토큰이 있던 없던, 루트 페이지로 이동한다.
+    this.$router.replace("/");
+  },
+};
+</script>
